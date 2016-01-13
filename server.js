f5097ee57c87543fd9e95d61be9a3275bde6cc33
Aaron Robb <aaronrobb@Sugarat.local>
2016-01-13 22:11:21 +1300

    Koa branch

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+'use strict'
 const koa = require('koa')
 const koaBody = require('koa-better-body')
 const Firebase = require('firebase')
+const FirebaseTokenGenerator = require("firebase-token-generator")
+const firebasePromisified = require('firebase-promisified')
 const router = require('koa-router')()
 const serve = require('koa-static')
 const thunkify = require('thunkify-wrap')
@@ -9,8 +12,15 @@ const dataRoot = new Firebase('https://grumble.firebaseio.com/')
 const port =  process.env.PORT || 5000
 const server = require('http').createServer(app.callback()).listen(port)
 const dotenv = require('dotenv')
+
+firebasePromisified(Firebase, Promise)
+
 const userData = new Firebase('https://grumble.firebaseio.com/users/')
+
 dotenv.load()
+
+
+const tokenGenerator = new FirebaseTokenGenerator(process.env.FIREBASE_SECRET)
 
 app.use(koaBody({
   extendTypes: {
@@ -23,13 +33,21 @@ app.use(koaBody({
 app.use(serve(__dirname + '/client'))
 
 router.get('/users/:id', function *(next) {
-  userData.child(this.params.id).on('value', (snapshot) => {
-    this.status = 200
-    this.body = snapshot.val()
-  }, (errorObject) => {
+  let response
+  let user
+  const token = tokenGenerator.createToken({uid: this.params.id, provider: 'github'})
+  userData.promiseAuthWithCustomToken(token)
 
-  })
-})
+  userData.child(this.params.id)
+    .promiseOnce('value')
+    .then( blah => {
+        this.body = blah.snapshot.val()
+        this.status = 200
+      })
+  this.status = 200
+  this.body = "lol"
+    })
+
 router.post('', function *(next) {
 
 })
