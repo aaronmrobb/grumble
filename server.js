8685399240a6b2dc5910616f52ae91bcc7f69efb
Aaron Robb <email": null>
2016-01-19 13:33:17 +1300

    blah

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 'use strict'
 const koa = require('koa')
 const koaBody = require('koa-better-body')
-const Firebase = require('firebase')
+const Firenext = require('Firenext')
 const FirebaseTokenGenerator = require("firebase-token-generator")
-const firebasePromisified = require('firebase-promisified')
 const router = require('koa-router')()
 const serve = require('koa-static')
 const thunkify = require('thunkify-wrap')
 const app = koa()
-const dataRoot = new Firebase('https://grumble.firebaseio.com/')
 const port =  process.env.PORT || 5000
 const server = require('http').createServer(app.callback()).listen(port)
 const dotenv = require('dotenv')
 
-firebasePromisified(Firebase, Promise)
 
-const userData = new Firebase('https://grumble.firebaseio.com/users/')
+const userData = new Firenext('https://grumble.firebaseio.com/users/')
 
 dotenv.load()
 
@@ -36,17 +33,18 @@ router.get('/users/:id', function *(next) {
   let response
   let user
   const token = tokenGenerator.createToken({uid: this.params.id, provider: 'github'})
-  userData.promiseAuthWithCustomToken(token)
+  userData.authWithCustomToken(token)
 
-  userData.child(this.params.id)
-    .promiseOnce('value')
-    .then( blah => {
-        this.body = blah.snapshot.val()
-        this.status = 200
-      })
+
   this.status = 200
-  this.body = "lol"
-    })
+  this.body = yield getUser(this.params.id)
+})
+
+function getUser(username) {
+  return userData.child(username).exec().then(function(snapshot){
+    return snapshot.val()
+  })
+}
 
 router.post('', function *(next) {
 
